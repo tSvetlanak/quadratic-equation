80ea266dd03d227870cc6410f8bf3cd2cfc83f8f
tSvetlanak <svetlana.a.k.15@mail.ru>
2018-09-26 17:52:00 +0300

    quadratic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+equation = equation.replace(/x/g, '').replace('^2', '').replace(/ /g, '');
+  equation = equation.split('*');
+
+  var a = parseFloat(equation[0]);
+  var b = parseFloat(equation[1]);
+  var c = parseFloat(equation[2]);
+  var arr = [];
+
+  var d = b * b - 4 * a * c;
+  if (d == 0) {
+    arr.push(Math.round( - (b / 2 * a)));
+  }
+  if (d > 0) {
+    arr.push(Math.round((- b + Math.sqrt(d)) / (2 * a)));
+    arr.push(Math.round((- b - Math.sqrt(d)) / (2 * a)));
+  }
+
+  return arr.sort(function(a, b) {return a - b;});
 }
